1e54859099485e7385ce4dac92653a27b370c42f
odashintaro <65642235+odashintaro@users.noreply.github.com>
2021-12-07 01:32:17 +0900

    --allow-empty

--- Movie/movie-recommend.py
+++ Movie/movie-recommend.py
@@ -5,11 +5,14 @@ from scipy.sparse import csr_matrix
 from sklearn.neighbors import NearestNeighbors
 
 #pandasライブラリで読み込み。自動的にカラム名設定。
-df = pd.read_csv("./ml-latest-small/ratings.csv", sep=",")
+df_rate = pd.read_csv("./ml-latest-small/ratings.csv", sep=",")
 #最初の0~2列目までを利用（ユーザーID、映画ID、評価の3列を利用）
-df = df.iloc[:,0:3]
+df_rate = df_rate.iloc[:,0:3]
+
+df_name = pd.read_csv("./ml-latest-small/movies.csv",sep=",")
+
 #疎行列(行列の中身が0ばかりの行列)に変換。行名が映画のID、列名がユーザーID、行列の中身が評価
-df_piv = df.pivot(index= "movieId",columns="userId",values="rating").fillna(0)
+df_piv = df_rate.pivot(index= "movieId",columns="userId",values="rating").fillna(0)
 #scikit-learnでの処理が速くなるデータ形式に変換
 df_sp = csr_matrix(df_piv.values)
 #KNNのインスタンス化。評価方法をコサイン類似度に設定。bruteは総当り方式。
@@ -20,10 +23,15 @@ rec_model = rec.fit(df_sp)
 # 映画ID「100」に対するおすすめの映画10個を類似度(下記のdistances)が近い順番に表示。
 #下記のindicesは各点から近い順にインデックスが入っており、行iには点iに近い順にインデックスが入っている。
 # 下記のn_neighbors=11は自身を含めて11個のデータを取り出す。
-Movie = 100#任意の好きな映画ID
-distance, indice = rec_model.kneighbors(df_piv.iloc[df_piv.index== Movie].values.reshape(1,-1),n_neighbors=11)
+Movieid = 100#任意の好きな映画ID
+distance, indice = rec_model.kneighbors(df_piv.iloc[df_piv.index== Movieid].values.reshape(1,-1),n_neighbors=11)
+
+#print(df_name.columns)
+#print(df_name.at[5,'title'])
+#print(df_name.at[Movieid, "title"])
 for i in range(11):#i=0は映画ID100のことであり、i=1~10までは評価が似ている映画のこと。
     if  i == 0:#i=0はID100のこと。
-        print("映画ID{}を見た人におすすめの映画IDは以下です。".format(Movie))
+        print("{}を見た人におすすめの映画は以下です。".format(df_name.at[Movieid, "title"]))
     else:
-        print("{0}: {1}".format(i,df_piv.index[indice.flatten()[i]]))#似ている順に映画IDを表示。
+        print("{}: {}".format(i,df_name.at[df_piv.index[indice.flatten()[i]], 'title']))
+        #print("{0}: {1}".format(i,df_piv.index[indice.flatten()[i]]))#似ている順に映画IDを表示。
